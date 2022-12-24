34064c805bdc23da89f8de1ae0f5f0455b4d5d49
joker <shridharjm1@gmail.com>
2022-12-24 18:55:38 +0530

    login page

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
-<?php
-echo "hello world";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Title</title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.15.19/dist/css/uikit.min.css" />
+
+    </head>
+
+    <body>
+
+    <div class="uk-section uk-container ">
+        <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+        <form class="uk-form-horizontal uk-margin-large js-login">
+
+<div class="uk-margin">
+    <label class="uk-form-label" for="form-horizontal-text">E-mail</label>
+    <div class="uk-form-controls">
+        <input class="uk-input" id="form-horizontal-text" type="email" placeholder="something@email.com" required>
+    </div>
+</div>
+
+<div class="uk-margin">
+    <label class="uk-form-label" for="form-horizontal-text">Password</label>
+    <div class="uk-form-controls">
+        <input class="uk-input" id="form-horizontal-text" type="password" placeholder="password">
+    </div>
+</div>
+
+<div class="uk-margin">
+    <button class="uk-button uk-button-default" type="submit">submit</div>
+</div>
+
+
+</form>
+        </div>
+    </div>
+
+
+
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.15.19/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.15.19/dist/js/uikit-icons.min.js"></script>
+
+    </body>
+</html>
